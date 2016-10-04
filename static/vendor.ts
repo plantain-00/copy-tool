d5565f0d82d952fd95751c602c8a124e1277d8fb
york yao <yaoao12306@outlook.com>
2016-10-04 15:06:30 +0800

    move vendor out from main.bundle.js

---
+++ static/vendor.ts
@@ -0,0 +1,6 @@
+import "core-js/es7/reflect";
+import "zone.js/dist/zone";
+import "tslib";
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
