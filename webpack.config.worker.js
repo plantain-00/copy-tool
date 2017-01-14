0e94ef61317999c92f635e4c408e694db0594831
york yao <yaoao12306@outlook.com>
2017-01-14 10:57:52 +0800

    fix CI

---
+++ webpack.config.worker.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        worker: "./static/worker"
+    },
+    output: {
+        filename: "static/[name].bundle.js"
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                "NODE_ENV": JSON.stringify("production")
+            }
+        }),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            },
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ["worker"]
+        })
+    ]
+};
