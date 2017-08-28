f1dbc65b25081323ee29e32daef0fa15e5262435
york yao <yaoao12306@outlook.com>
2017-08-28 21:00:28 +0800

    better screenshot

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -24,17 +24,19 @@ module.exports = {
       async () => {
         const puppeteer = require('puppeteer')
         const fs = require('fs')
+        const beautify = require('js-beautify').html
         const server = childProcess.spawn('node', ['index.js'])
         server.stdout.pipe(process.stdout)
         server.stderr.pipe(process.stderr)
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
+        await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
         await page.waitFor(1000)
         await page.goto(`http://localhost:8000/#test`)
         await page.waitFor(1000)
         await page.screenshot({ path: `static/screenshot.png`, fullPage: true })
         const content = await page.content()
-        fs.writeFileSync(`static/screenshot-src.html`, content)
+        fs.writeFileSync(`static/screenshot-src.html`, beautify(content))
         server.kill('SIGINT')
         browser.close()
       }
@@ -94,6 +96,7 @@ module.exports = {
       server.stderr.pipe(process.stderr)
       const browser = await puppeteer.launch()
       const page = await browser.newPage()
+      await page.emulate({ viewport: { width: 1440, height: 900 }, userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' })
       await page.waitFor(1000)
       await page.goto('http://localhost:8000/#test')
       await page.waitFor(1000)
