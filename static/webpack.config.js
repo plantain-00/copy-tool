b80473d00486905c08e29b3c6574943f5dd036d2
york yao <yaoao12306@outlook.com>
2018-03-04 19:10:03 +0800

    chore: update dependencies

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -3,53 +3,31 @@ const webpack = require('webpack')
 module.exports = [
   {
     entry: {
-      index: './static/index',
-      vendor: './static/vendor'
+      index: './static/index'
     },
     output: {
       path: __dirname,
       filename: '[name].bundle.js'
     },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('production')
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all'
+          }
         }
-      }),
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        output: {
-          comments: false
-        },
-        exclude: [
-        ]
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: ['index', 'vendor']
-      })
-    ]
+      }
+    }
   },
   {
     entry: {
       worker: './static/worker'
     },
     output: {
-      filename: 'static/[name].bundle.js'
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('production')
-        }
-      }),
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        output: {
-          comments: false
-        },
-        exclude: [
-        ]
-      })
-    ]
+      path: __dirname,
+      filename: '[name].bundle.js'
+    }
   }
 ]
